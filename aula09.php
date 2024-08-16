352854ebbeba7c97be52d3334f72db3d7e9a8f78
MariaLuizaVasconcelos <163915183+MariaLuizaVasconcelos@users.noreply.github.com>
2024-08-16 18:21:39 -0300

    Add files via upload

---
+++ aula09.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AULA 09</title>
+</head>
+<body>
+    <form action="calcular.php" method="post">
+        <label for="n1"> Número 1 : </label>
+        <input type= "text" name="n1" ><br>
+        <label for="n2"> Número 2 : </label>
+        <input type= "text" name="n2" ><br>
+        <input type="submit" value="Somar">
+
+</form>
+</body>
+</html>
